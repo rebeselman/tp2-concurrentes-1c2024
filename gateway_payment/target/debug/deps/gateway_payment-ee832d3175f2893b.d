d5b4632c559e315e8e524eb7facd6aa5b8e093dc
juanimedone <jmedone@fi.uba.ar>
2024-06-04 12:50:55 -0300

    elimino targets

--- gateway_payment/target/debug/deps/gateway_payment-ee832d3175f2893b.d
+++
@@ -1,5 +0,0 @@
-/home/rebeselman/programación-concurrente/tp2/gateway_payment/target/debug/deps/libgateway_payment-ee832d3175f2893b.rmeta: src/main.rs
-
-/home/rebeselman/programación-concurrente/tp2/gateway_payment/target/debug/deps/gateway_payment-ee832d3175f2893b.d: src/main.rs
-
-src/main.rs:
